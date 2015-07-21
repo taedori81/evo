8e9b539cd8b10951d341f6768ae1e3d160bab147
Tae Hwan Kim <taedori@outlook.com>
2015-07-20 19:52:54 -0700

    Changing allowed_hosts

--- evo/settings/production.py
+++ evo/settings/production.py
@@ -18,7 +18,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 # }
 
 
-ALLOWED_HOSTS = ['.byevo.com']
+ALLOWED_HOSTS = ['*']
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_HOST_USER = env('EMAIL_ADDRESS')
